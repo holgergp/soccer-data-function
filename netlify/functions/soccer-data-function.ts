0292b0ae315f959c0c12af3814bb962f96d11da4
Holger Grosse-Plankermann <holgergp@gmail.com>
2022-08-17 15:33:19 +0200

    Initial version of soccer-data-function

---
+++ netlify/functions/soccer-data-function.ts
@@ -0,0 +1,15 @@
+import { Handler } from "@netlify/functions";
+import { SAMPLE_LEAGUE_TABLE } from "./sampleData";
+
+const handler: Handler = async (event) => {
+  if (event.httpMethod === "GET") {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(SAMPLE_LEAGUE_TABLE),
+    };
+  } else {
+    return { statusCode: 405 };
+  }
+};
+
+export { handler };
